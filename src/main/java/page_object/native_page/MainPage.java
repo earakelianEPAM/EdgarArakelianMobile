086ad3fc7c4382b7bce7294fcf0f7f7470a5f061
Edgar_Arakelian <earakelian@idbs.com>
2023-02-23 12:58:34 +0100

    HW3 - fixed

---
+++ src/main/java/page_object/native_page/MainPage.java
@@ -0,0 +1,8 @@
+package page_object.native_page;
+
+import io.appium.java_client.AppiumDriver;
+
+public class MainPage {
+    protected AppiumDriver appiumDriver;
+    protected final static String APPLIC_NAME_ID = "platkovsky.alexey.epamtestapp:id/";
+}
